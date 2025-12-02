2aae2d7e4fcd418cfb1e145ea1553c2049b5bd8c
Marcelo Torres <marcelo@marcelotorresc.com>
2025-12-01 23:55:52 -0300

    final commit

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ model = joblib.load(MODEL_PATH)
 
 @app.get("/health")
 def health():
-    return {"status": "ok" , "version": "v202"}
+    return {"status": "ok" , "version": "vfinal"}
 
 
 @app.post("/predict")
